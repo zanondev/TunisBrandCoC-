379b05193fc58294b5c73449766ba8a354b2c765
Lucas <szanonn@gmail.com>
2022-11-21 21:57:07 -0300

    adicionando as entidades

--- TunisBrandCo.Domain/Entities/Product.cs
+++ TunisBrandCo.Domain/Entities/Product.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace TunisBrandCo.Domain.Entities
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public int StockQuantity { get; set; }
+        public bool IsActive { get; set; }
+
+        public Product(int id, decimal price, DateTime expiryDate, int stockQuantity, bool isActive)
+        {
+            Id = id;
+            Price = price;
+            ExpiryDate = expiryDate;
+            StockQuantity = stockQuantity;
+            IsActive = isActive;
+            isActive = true;
+        }
     }
 }
